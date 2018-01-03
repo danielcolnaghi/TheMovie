33c987a1cc1e763cd6413aea6fcfcbc503847a21
Daniel Colnaghi <danielcolnaghi@gmail.com>
2018-01-03 11:48:44 -0200

    Working with dynamics in movie cell.

--- DCTheMovie/DCTheMovie/View/Cells/MovieCell.swift
+++ DCTheMovie/DCTheMovie/View/Cells/MovieCell.swift
@@ -14,11 +14,36 @@ class MovieCell : UITableViewCell {
 	@IBOutlet weak var lblTitle: UILabel!
 	@IBOutlet weak var lblVote: UILabel!
 	@IBOutlet weak var lblReleaseDate: UILabel!
-	
+    @IBOutlet weak var vAnchor: UIView!
+    
     private var movieId = 0
     
+    // Dynamics
+    private var gravity: UIGravityBehavior!
+    private var animator: UIDynamicAnimator!
+    private var attachment: UIAttachmentBehavior!
+    private var collision: UICollisionBehavior!
+    private var dynamicItem: UIDynamicItemBehavior!
+    
 	func loadCellWithMovie(_ movie : Movie) {
+        
+        // Behaviors
+        attachment = UIAttachmentBehavior(item: self.imgBackground, attachedToAnchor: CGPoint(x: vAnchor.center.x, y: vAnchor.center.y))
+        gravity = UIGravityBehavior(items: [self.imgBackground])
+        collision = UICollisionBehavior(items: [self.imgBackground])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        dynamicItem = UIDynamicItemBehavior(items: [self.imgBackground])
+        dynamicItem.allowsRotation = true
 
+        animator = UIDynamicAnimator(referenceView: self)
+        animator.addBehavior(dynamicItem)
+        animator.addBehavior(gravity)
+        animator.addBehavior(attachment)
+//        animator.addBehavior(collision)
+        
+        
+        
+        
         self.lblTitle.text = movie.title
         self.lblVote.text = "Vote Avarage \(movie.voteAvarage)"
         self.lblReleaseDate.text = "\(movie.releaseDate)"
